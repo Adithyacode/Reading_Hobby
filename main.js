c5f6cdf5bf6f218f0f60a2ef40fca93528cb9b26
Adithyacode <86084445+Adithyacode@users.noreply.github.com>
2021-07-08 13:52:40 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function My_Function(){
+    document.getElementById("text_1").innerHTML="Hello, I'm here to tell you about my favorite hobby: Reading! So what is reading? Reading is the process of taking in the sense or meaning of letters, symbols, etc. Reading came from writing with a few exceptions like the Athens and the Caliphate. Readin and writing go hand in hand with eachother, so if you get better at readin (or writing), you get better at writing (or reading) too! I hope you learned a thing or 2 in this passage and I'll see you later!";
+};
+
+function My_Function2(){
+    document.getElementById("text_1").innerHTML="";
+};
